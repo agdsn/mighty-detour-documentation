d924935609bfcc35357522248b6991facd334c50
Hannibal42 <hanischsimon@gmail.com>
2017-02-08 18:24:03 +0100

    update the client and server

--- tests/python/clientNew.py
+++ tests/python/clientNew.py
@@ -1,28 +1,57 @@
+from gevent.server import DatagramServer
 import sys
+import time
+import json
 from gevent import socket
 import ipaddress
 import itertools
 import gevent
+import os
 
 
 BASE_PORT = int(sys.argv[2])
 NETWORK = ipaddress.ip_network(sys.argv[1])
+NETWORKS = NETWORK.subnets(new_prefix=24)
 
 
-def getIP(id):
-    return str(next(itertools.islice(NETWORK.hosts(), id, id + 1)))
+class ReceiveServer(DatagramServer):
 
-def setUpUDP(id):
-    address = (sys.argv[3], int(sys.argv[4]))
-    message = 'Hello World! ' + str(id)
+    def handle(self, data, address):
+        print("Handle")
+        tmp = time.time()
+        payload = json.loads(data.decode())
+        payload["pongRecv"] = tmp
+        print(payload)
+
+
+def spawnServer(port):
+    print("Server up on port: " + str(500 + port))
+    ReceiveServer(':' + str(port + 500)).serve_forever()
+
+def getIP(net, id):
+    return str(next(itertools.islice(net.hosts(), id, id + 1)))
+
+def setUpUDP(net, port, id):
+    address = (sys.argv[3], port)
+
     s = socket.socket(type=socket.SOCK_DGRAM)
-    tmp = (getIP(id), BASE_PORT)
+    tmp = (getIP(net, id), port)
     s.bind(tmp)
     s.connect(address)
+    print(tmp)
+    #ReceiveServer(':' + str(port + 500)).serve_forever()
+    #print("Server up")
     while True:
-        s.send(message.encode())
-        data, address = s.recvfrom(8192)
-        print(data.decode())
+        payload = {"id": id, "ping": time.time()}
+        s.send(json.dumps(payload).encode())
+        gevent.sleep()
 
-connections = [gevent.spawn(setUpUDP, i) for i in range(10)]
+connections = []
+servers = []
+port = BASE_PORT
+for net in NETWORKS:
+    servers += [gevent.spawn(spawnServer, port)]
+    connections += [gevent.spawn(setUpUDP, net, port, i) for i in range(10)]
+    port += 1
+
 gevent.joinall(connections)
--- tests/python/serverNew.py
+++ tests/python/serverNew.py
@@ -1,14 +1,42 @@
 import sys
+import time
+import json
+from gevent import socket
 from gevent.server import DatagramServer
+import gevent
+import gevent.queue
 
 PORT = sys.argv[1]
 
-
 class EchoServer(DatagramServer):
 
+    queue = gevent.queue.Queue()
+
+    def __init__(self, port):
+        gevent.spawn(self.sendReplys)
+        super(DatagramServer, self).__init__(port)
+
+    def sendReplys(self):
+
+        s = socket.socket(type=socket.SOCK_DGRAM)
+        s.bind(("192.168.0.34", 9001))
+        for item in self.queue:
+            ip, port = item['address']
+            port += 500
+            s.connect((ip, port))
+            item['message']["pongSend"] = time.time()
+            print("Send response to:" + str(ip) + " " + str(port))
+            s.sendall(json.dumps(item['message']).encode())
+
+        s.shutdown(socket.SHUT_WR)
+        s.close()
+
+
     def handle(self, data, address):
-        self.socket.sendto(('Received %s bytes' % len(data)).encode('utf-8'), address)
-        print("Pong")
+        tmp = time.time()
+        payload = json.loads(data.decode())
+        payload["pingRecv"] = tmp
+        self.queue.put({ 'message': payload, 'address': address})
 
 
 if __name__ == '__main__':
