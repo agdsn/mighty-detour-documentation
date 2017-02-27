04166865f568d630191d54c4721cfb192fda3386
Felix Kluge <f.kluge@agdsn.de>
2017-02-27 19:57:04 +0100

    Do not use grep anymore, but let python parse the nft output for rule matching

--- nft/rules.py
+++ nft/rules.py
@@ -28,22 +28,26 @@ def rule_exists(value, table, chain=None):
     logging.debug("Check if a rule in table %s chain %s contains %s", table, chain, value)
     if chain is None:
         logging.debug("No chain specified, searching in whole nft table %s", table)
-        command = cfg()['netfilter']['nft']['call'] + " list table " + table + " -a -nnn | /bin/grep '" + str(value) + "'"
+        command = cfg()['netfilter']['nft']['call'] + " list table " + table + " -a -nnn"
     else:
-        command = cfg()['netfilter']['nft']['call'] + " list chain " + table + " " + chain + " -a -nnn | /bin/grep '" + str(value) + "'"
+        command = cfg()['netfilter']['nft']['call'] + " list chain " + table + " " + chain + " -a -nnn"
     logging.debug("Execute: " + command)
-    output = subprocess.check_output(command, shell=True, stderr=subprocess.STDOUT).decode("utf-8").replace("\\t", "").replace("\\n", "").splitlines()
-    if len(output) > 1:
+    output = subprocess.check_output(command, shell=True).decode("utf-8").replace("\\t", "").replace("\\n", "").splitlines()
+    matched_output = []
+    for l in output:
+        if str(value) in l:
+            matched_output.append(l.strip())
+    if len(matched_output) > 1:
         logging.warning("There are multiple rules present in table %s chain %s that contain %s", table, chain, value)
-        for o in output:
+        for o in matched_output:
             logging.warning("Rule: %s", o)
-    elif len(output) == 0:
+    elif len(matched_output) == 0:
         logging.debug("There is no rule that contains %s in table %s chain %s", value, table, chain)
         return False
-    if "handle" not in output[0]:
+    if "handle" not in matched_output[0]:
         logging.warning("There is no handle for %s in table %s chain %s in the first matched output line", value, table, chain)
         return False
-    parsed_handle = output[0].split(" # handle ")
+    parsed_handle = matched_output[0].split(" # handle ")
     # Return the handle number
     logging.debug("The handle for the rule that contains %s in table %s chain %s is %s",
                   value, table, chain, parsed_handle[1])
